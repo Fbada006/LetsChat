e72cce119d0ddb516dc67ba8bf11c84d65768039
Fbada006 <ferdinandkamata@yahoo.com>
2021-04-05 12:15:48 +0300

    add landing fragment to show nav

--- app/src/main/java/com/example/letschat/MainActivity.kt
+++ app/src/main/java/com/example/letschat/MainActivity.kt
@@ -1,13 +1,43 @@
 package com.example.letschat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val host = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.landingFragment
+            )
+        )
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
