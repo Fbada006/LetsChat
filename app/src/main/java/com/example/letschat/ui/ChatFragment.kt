9f4c82f7e8e3cdb42c152674c6a4cef7e0ab5390
Fbada006 <ferdinandkamata@yahoo.com>
2022-04-19 19:49:31 +0300

    properly initialise and encrypt messages with ably

--- app/src/main/java/com/example/letschat/ui/ChatFragment.kt
+++ app/src/main/java/com/example/letschat/ui/ChatFragment.kt
@@ -1,37 +1,61 @@
 package com.example.letschat.ui
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DefaultItemAnimator
-import com.example.letschat.R
 import com.example.letschat.databinding.ChatFragmentBinding
+import com.example.letschat.utils.AblyConnection
+import com.example.letschat.utils.AblyConnectionCallback
 import com.example.letschat.utils.Resource
 import com.example.letschat.utils.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
+import io.ably.lib.realtime.Channel
+import io.ably.lib.types.AblyException
+import io.ably.lib.types.Message
+import timber.log.Timber
 
 
 @AndroidEntryPoint
 class ChatFragment : Fragment() {
 
+    private val choices = arrayOf(true, false)
     private val viewModel: ChatViewModel by viewModels()
     private var _binding: ChatFragmentBinding? = null
     private val binding get() = _binding!!
     private lateinit var messagesAdapter: MessagesAdapter
+    private val messageListener: Channel.MessageListener =
+        Channel.MessageListener { message: Message ->
+            viewModel.saveMessage(
+                message.data.toString(),
+                choices.random()
+            )
+        }
+    private val connectionCallback = object : AblyConnectionCallback {
+        override fun onConnectionCallback(exception: Exception?) {
+            try {
+                if (exception == null) {
+                    AblyConnection.initListener(messageListener)
+                } else {
+                    Timber.e("Error initialising listener with a non null exception ", exception)
+                }
+            } catch (exception: AblyException) {
+                Timber.e("Error initialising listener ", exception)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        AblyConnection.establishConnectionForID("userNameHere", connectionCallback)
         _binding = ChatFragmentBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -42,22 +66,19 @@ class ChatFragment : Fragment() {
         viewModel.getAllMessages()
 
         binding.fabSend.setOnClickListener {
-            val slideOutBottom: Animation = AnimationUtils.loadAnimation(
-                context,
-                R.anim.out_bottom
-            )
-            slideOutBottom.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {
-                }
-
-                override fun onAnimationEnd(animation: Animation?) {
-                    displaySendChoice()
-                }
-
-                override fun onAnimationRepeat(animation: Animation?) {
-                }
-            })
-            binding.layoutMessageInput.startAnimation(slideOutBottom)
+            try {
+                val message = binding.etMessage.text.toString()
+                AblyConnection.sendMessage(message, object : AblyConnectionCallback {
+                    override fun onConnectionCallback(exception: Exception?) {
+                        if (exception != null) {
+                            Timber.e("Error sending message", exception)
+                        }
+                        activity?.runOnUiThread { clearTextAndHideKeyboard() }
+                    }
+                })
+            } catch (exception: AblyException) {
+                Timber.e("Error in clicking send button", exception)
+            }
         }
 
         binding.etMessage.addTextChangedListener {
@@ -69,24 +90,6 @@ class ChatFragment : Fragment() {
         }
 
         initRecyclerView()
-    }
-
-    private fun displaySendChoice() {
-        val builder = AlertDialog.Builder(context)
-        builder.setTitle(getString(R.string.send_as))
-
-        val choices =
-            arrayOf(getString(R.string.send_as_me_label), getString(R.string.send_as_not_me_label))
-        builder.setItems(choices) { dialog, which ->
-            dialog.dismiss()
-            val message = binding.etMessage.text.toString()
-            clearTextAndHideKeyboard()
-            val isMine = choices[which] == getString(R.string.send_as_me_label)
-            viewModel.saveMessage(message, isMine)
-        }
-
-        val dialog = builder.create()
-        dialog.show()
     }
 
     private fun clearTextAndHideKeyboard() {
