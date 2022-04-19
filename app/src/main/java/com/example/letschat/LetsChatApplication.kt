33b816fa757b6d6353d054176783c152f360a584
Fbada006 <ferdinandkamata@yahoo.com>
2022-04-19 13:33:20 +0300

    add timber for better logging

--- app/src/main/java/com/example/letschat/LetsChatApplication.kt
+++ app/src/main/java/com/example/letschat/LetsChatApplication.kt
@@ -2,6 +2,17 @@ package com.example.letschat
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 @HiltAndroidApp
-class LetsChatApplication : Application()
+class LetsChatApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
